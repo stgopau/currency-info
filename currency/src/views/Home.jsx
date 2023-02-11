7c04bc2d5b2ea21d888260442048efddaa11e2c9
stgopau <spau@uc.cl>
2023-02-11 18:57:41 -0300

    card ready

--- currency/src/views/Home.jsx
+++ currency/src/views/Home.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Card from '../components/home/Card';
 
 export default function Home() {
+  const currencies = ['dolar', 'euro', 'bitcoin', 'uf'];
   return (
-    <div className="bg-[#21242a] flex w-screen h-screen p-20">
-      <header className="flex justify-between w-full">
-        <h1 className="text-6xl text-[#1dca7f] font-['Roboto Mono'] font-bold ">Money Data</h1>
+    <div className="bg-[#21242a] flex flex-col w-screen h-screen p-20">
+      <header className="flex mb-20">
+        <h1 className="text-6xl text-[#1dca7f] font-bold ">Money Data</h1>
       </header>
-      <Card
-        name="Dolar"
-        valueToday="5.000"
-        valueYesterday="4.500"
-      />
+      <div className="flex grid grid-cols-4 gap-12">
+        {currencies.map((currency) => (
+          <Card
+            name={currency}
+            valueToday="5.000"
+            valueYesterday="4.500"
+          />
+        ))}
+      </div>
     </div>
   );
 }
